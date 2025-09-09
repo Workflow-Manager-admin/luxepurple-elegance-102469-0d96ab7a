aa4e279a9c698e09c4576ff1baec77bfc591d17d
Kavia AI <production@kavia.ai>
2025-09-09 09:31:45 +0000

    CheckPoint - cgc898b2a1

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/luxepurple-elegance-102469-0d96ab7a/MainContainerforLuxePurpleElegance
+npm run build
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
